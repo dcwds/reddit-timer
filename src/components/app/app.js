c9bda450924bbab512d1650f163e725dd91c3e78
dcwds <dcwds@protonmail.com>
2021-03-27 17:32:55 -0400

    deps: add react-router-dom and styled-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import '../../styles/normalize.css';
 import '../../styles/global.css';
 
-function App() {
-  return <div>App Placeholder</div>;
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <div>
+          <code>/</code>
+          {' '}
+          placeholder
+        </div>
+      </Route>
+      <Route path="/search">
+        <div>
+          <code>/search</code>
+          {' '}
+          placeholder
+        </div>
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
