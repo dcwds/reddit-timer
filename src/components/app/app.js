4e661f678596ea13823c79a364acf496751e0a39
dcwds <dcwds@protonmail.com>
2021-03-27 17:51:39 -0400

    styles and routing: remove old normalize css and add routes for / and /search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '../../styles/normalize.css';
+import 'normalize.css';
 import '../../styles/global.css';
 
 const App = () => (
