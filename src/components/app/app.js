0d6f01bf0ff576c73e9dca51305ed3ba718f6954
dcwds <dcwds@protonmail.com>
2021-03-28 12:17:03 -0400

    header: add component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
 import GlobalStyle from '../global-style';
+import Header from '../header';
 
 const App = () => (
   <Router>
     <Normalize />
     <GlobalStyle />
+
+    <Header />
     <Switch>
       <Route exact path="/">
         Home
