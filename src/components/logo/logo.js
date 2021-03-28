09c43a974f78f02ff952c93c69e2b5b42a7ad769
dcwds <dcwds@protonmail.com>
2021-03-28 12:16:32 -0400

    logo: import images instead of using path

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoFull from './logo-full.png';
+import logoMark from './logo-mark.png';
 
 const Logo = ({ asMark = false }) => (
   <Link to="/">
-    <img src={`./logo-${asMark ? 'mark' : 'full'}.png`} alt="logo" />
+    <img src={asMark ? logoMark : logoFull} alt="logo" />
   </Link>
 );
 
