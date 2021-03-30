61f213c521521ebac0c6b51266f55c6798f2af99
dcwds <dcwds@protonmail.com>
2021-03-29 21:46:20 -0400

    styles: use flex-grow instead of justify-content: space-between on footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Logo from '../logo';
 import * as S from './footer.style';
 
 const Footer = () => (
   <S.Footer>
-    <a href="https://profy.dev/employers">profy.dev</a>
+    <S.ProfyLink href="https://profy.dev/employers">profy.dev</S.ProfyLink>
     <Logo asMark />
-    <Link to="/terms">Terms & Privacy</Link>
+    <S.TermsLink to="/terms">Terms & Privacy</S.TermsLink>
   </S.Footer>
 );
 
--- src/components/footer/footer.style.js
+++ src/components/footer/footer.style.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Footer = styled.footer`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   height: 6.25rem; /* 100px */
   font-size: 0.875rem;
+`;
+
+export const ProfyLink = styled.a`
+  flex: 1;
+`;
+
+export const TermsLink = styled(Link)`
+  flex: 1;
+  text-align: right;
 `;
