977af3f206fab5364e28c32a4949ac8a16bf01c0
dcwds <dcwds@protonmail.com>
2021-04-15 16:31:15 -0400

    hooks: clean up variables in fetchPosts fn in useFetchPosts

--- src/hooks/use-fetch-posts/use-fetch-posts.js
+++ src/hooks/use-fetch-posts/use-fetch-posts.js
@@ -4,25 +4,20 @@ import { useParams } from 'react-router-dom';
 export const fetchPosts = async (
   subreddit,
   postAmount = 500,
-  lastPostId,
-  fetchedPosts,
+  lastPostId = null,
+  fetchedPosts = [],
 ) => {
-  let postId = typeof (lastPostId) === 'undefined' ? null : lastPostId;
-  let posts = typeof (fetchedPosts) === 'undefined' ? [] : fetchedPosts;
-  let noMorePosts = false;
+  const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=100&after=${lastPostId}`);
 
-  const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=100&after=${postId}`);
-
-  if (!response.ok) return posts;
+  if (!response.ok) return fetchedPosts;
 
   const { data: { children, after } } = await response.json();
-
-  postId = after;
-  posts = posts.concat(children);
-  noMorePosts = children.length < 100; // based on `limit` parameter in url
+  const posts = fetchedPosts.concat(children);
+  const noMorePosts = children.length < 100; // based on `limit` parameter in url
 
   if (posts.length >= postAmount || noMorePosts) return posts.slice(0, postAmount);
-  return fetchPosts(subreddit, postAmount, postId, posts);
+
+  return fetchPosts(subreddit, postAmount, after, posts);
 };
 
 const useFetchPosts = () => {
