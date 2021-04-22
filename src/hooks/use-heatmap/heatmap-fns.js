920aa0d65acaceb32cb24f01d2e297d7edfcf119
dcwds <dcwds@protonmail.com>
2021-04-22 09:04:02 -0400

    hooks, tests: update Heatmap fns to include postIds within weekday hour blocks to allow posts to be filtered

--- src/hooks/use-heatmap/heatmap-fns.js
+++ src/hooks/use-heatmap/heatmap-fns.js
@@ -2,22 +2,31 @@ export const weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
 export const weekdayMap = weekdays.reduce((map, curr) => ({ ...map, [curr]: [] }), {});
 export const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-export const updateWeekdayHours = (hours, hour) => {
+export const updateWeekdayHours = (hours, post) => {
+  const { hour, id } = post;
   const foundHour = hours.find((h) => h.hour === hour);
 
   if (foundHour) {
-    const { hour: hr, count } = foundHour;
+    const { hour: hr, count, postIds } = foundHour;
 
-    return [...hours.filter((h) => h.hour !== hr), { hour: hr, count: count + 1 }];
+    return [
+      ...hours.filter((h) => h.hour !== hr),
+      {
+        hour: hr,
+        count: count + 1,
+        postIds: postIds.concat(id),
+      },
+    ];
   }
 
-  return [...hours, { hour, count: 1 }];
+  return [...hours, { hour, count: 1, postIds: [id] }];
 };
 
 export const postsByWeekdayAndHour = (posts) => posts.map((p) => {
   const date = new Date(p.data.created_utc * 1000);
 
   return {
+    id: p.data.id,
     weekday: weekdays[date.getDay()],
     hour: date.getHours(),
   };
@@ -26,5 +35,5 @@ export const postsByWeekdayAndHour = (posts) => posts.map((p) => {
 export const getHeatmapFromPosts = (posts) => postsByWeekdayAndHour(posts)
   .reduce((days, curr) => ({
     ...days,
-    [curr.weekday]: updateWeekdayHours(days[curr.weekday] || [], curr.hour),
+    [curr.weekday]: updateWeekdayHours(days[curr.weekday], curr),
   }), weekdayMap);
--- src/hooks/use-heatmap/use-heatmap.test.js
+++ src/hooks/use-heatmap/use-heatmap.test.js
@@ -8,34 +8,40 @@ describe('useHeatmap', () => {
 
   it('converts the unix timestamp of posts', () => {
     expect(postsByWeekdayAndHour(mockPosts)).toEqual([
-      { hour: 13, weekday: 'saturday' },
-      { hour: 13, weekday: 'sunday' },
-      { hour: 13, weekday: 'monday' },
+      { hour: 13, id: '1', weekday: 'saturday' },
+      { hour: 13, id: '2', weekday: 'sunday' },
+      { hour: 13, id: '3', weekday: 'monday' },
     ]);
   });
 
   it('creates a heatmap from posts by weekdays and hours', () => {
     expect(getHeatmapFromPosts(mockPosts)).toEqual({
-      sunday: [{ hour: 13, count: 1 }],
-      monday: [{ hour: 13, count: 1 }],
+      sunday: [{ hour: 13, count: 1, postIds: ['2'] }],
+      monday: [{ hour: 13, count: 1, postIds: ['3'] }],
       tuesday: [],
       wednesday: [],
       thursday: [],
       friday: [],
-      saturday: [{ hour: 13, count: 1 }],
+      saturday: [{ hour: 13, count: 1, postIds: ['1'] }],
     });
   });
 
   it('should add new hour to hours object with a count of 1', () => {
-    expect(updateWeekdayHours([{ hour: 12, count: 1 }], 13)).toEqual([
-      { hour: 12, count: 1 },
-      { hour: 13, count: 1 },
+    expect(updateWeekdayHours(
+      [{ hour: 12, count: 1, postIds: ['1'] }],
+      { hour: 13, id: '2' },
+    )).toEqual([
+      { hour: 12, count: 1, postIds: ['1'] },
+      { hour: 13, count: 1, postIds: ['2'] },
     ]);
   });
 
   it('should increment hour count by 1 when hour already exists', () => {
-    expect(updateWeekdayHours([{ hour: 12, count: 1 }], 12)).toEqual([
-      { hour: 12, count: 2 },
+    expect(updateWeekdayHours(
+      [{ hour: 12, count: 1, postIds: ['1'] }],
+      { hour: 12, id: '3' },
+    )).toEqual([
+      { hour: 12, count: 2, postIds: ['1', '3'] },
     ]);
   });
 });
